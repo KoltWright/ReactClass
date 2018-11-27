498249cdc8b43950de485ae6de260f2516a0c71a
Kolt Wright <tywright557@gmail.com>
2018-11-26 22:26:15 -0700

    Completed components application and started with seasons component

--- components/src/index.js
+++ components/src/index.js
@@ -1,41 +1,45 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import faker from 'faker';
+import React from "react";
+import ReactDOM from "react-dom";
+import faker from "faker";
 
-import CommentDetial from './CommentDetail';
+import CommentDetial from "./CommentDetail";
+import ApprovalCard from "./ApprovalCard";
 
 const App = () => {
-    return ( 
-        <div className = "ui container comments" >
-        <CommentDetial 
-        avatar = {
-            faker.image.avatar()
-        }
-        author = "Sam"
-        timeAgo = "Today at 4:45PM"
-        comment = "Nice blog post!" 
-        />
-        <CommentDetial 
-            avatar = {
-                faker.image.avatar()
-            }
-            author = "Alex"
-            timeAgo = "Today at 2:00AM"
-            comment = "Sweet brohime!" 
-        />
-        <CommentDetial 
-            avatar = {
-                faker.image.avatar()
-            }
-            author = "Jane"
-            timeAgo = "Yesterday at 5:00PM"
-            comment = "Dalfinately!" 
-        />
+  return (
+    <div className="ui container comments">
+      <ApprovalCard>
+        <div>
+          <h4>Warning!</h4>
+          Are you sure you want to do this?
         </div>
-    );
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetial
+          avatar={faker.image.avatar()}
+          author="Sam"
+          timeAgo="Today at 4:45PM"
+          comment="Nice blog post!"
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetial
+          avatar={faker.image.avatar()}
+          author="Alex"
+          timeAgo="Today at 2:00AM"
+          comment="Sweet brohime!"
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetial
+          avatar={faker.image.avatar()}
+          author="Jane"
+          timeAgo="Yesterday at 5:00PM"
+          comment="Dalfinately!"
+        />
+      </ApprovalCard>
+    </div>
+  );
 };
 
-ReactDOM.render( <
-    App / > ,
-    document.querySelector('#root')
-);
+ReactDOM.render(<App />, document.querySelector("#root"));
--- jsx/src/index.js
+++ jsx/src/index.js
@@ -1,26 +1,23 @@
 // Import the React and ReactDOM libraries
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
 // Create a react component
 const App = () => {
-    const buttonText = {text: 'Click me'};
-    const labelText = 'Enter name:';
+  const buttonText = { text: "Click me" };
+  const labelText = "Enter name:";
 
-    return (
-        <div>
-            <label className="label" htmlFor="name">
-                {labelText}
-            </label>
-            <input id="name" type="text" />
-            <button style={{ backgroundColor: 'blue', color: 'white'}}>
-                {buttonText.text}
-            </button>
-        </div>
-    )
-}
+  return (
+    <div>
+      <label className="label" htmlFor="name">
+        {labelText}
+      </label>
+      <input id="name" type="text" />
+      <button style={{ backgroundColor: "blue", color: "white" }}>
+        {buttonText.text}
+      </button>
+    </div>
+  );
+};
 
-ReactDOM.render(
-    <App />,
-    document.querySelector('#root')
-);
+ReactDOM.render(<App />, document.querySelector("#root"));
