2e7b0727439e4571ed8a777bb5e7989f92a02451
kwright <kwright@adaweb.net>
2018-12-05 17:13:03 -0700

    Started work on blog component

---
+++ blog/src/actions/index.js
@@ -0,0 +1,18 @@
+import _ from "lodash";
+
+import jsonPlaceholder from "../apis/jsonPlaceholder";
+
+export const fetchPosts = () => {
+  return async dispatch => {
+    const res = await jsonPlaceholder.get("/posts");
+
+    dispatch({ type: "FETCH_POSTS", payload: res.data });
+  };
+};
+
+export const fetchUser = id => dispatch => _fetchUser(id, dispatch);
+const _fetchUser = _.memoize(async (id, dispatch) => {
+  const res = await jsonPlaceholder.get(`/users/${id}`);
+
+  dispatch({ type: "FETCH_USER", payload: res.data });
+});
